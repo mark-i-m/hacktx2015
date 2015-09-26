7b3e78489cce92cd9791b71059b976fd68e3838c
Mark Mansi <mmansi@utexas.edu>
2015-09-26 13:37:18 -0500

    Got express working

--- app.js
+++ app.js
@@ -3,19 +3,12 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
-
+var express = require('express');
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
@@ -27,10 +20,17 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
-
-app.get('/', routes.index);
-
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+
+// Add request handling here
+
+app.get('/app', function (req, res) {
+    res.render('index.html');
+});
+
+app.post('/app/:id', function (req, res) {
+    //console.log(req.params);
+    //console.log(req.query);
+} );
