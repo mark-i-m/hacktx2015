8de2130733cf7ddd7a9e9606825aba6cea098642
Mark Mansi <mmansi@utexas.edu>
2015-09-26 15:11:53 -0500

    Working on adding google drive code

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-
 /**
  * Module dependencies.
  */
 
+// Node express
 var express = require('express');
 var app = module.exports = express.createServer();
 
-// Configuration
+// Cloud
+var gdrive = require('./googledrive.js');
+var dbox = require('./dropbox.js');
 
+/**
+ * Configuration
+ */
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
@@ -24,8 +29,9 @@ app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
-// Add request handling here
-
+/**
+ * Request handling and routing
+ */
 app.get('/app', function (req, res) {
     res.render('index.html');
 });
@@ -33,4 +39,4 @@ app.get('/app', function (req, res) {
 app.post('/app/:id', function (req, res) {
     //console.log(req.params);
     //console.log(req.query);
-} );
+});
